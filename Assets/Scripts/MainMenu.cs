037f90f9da624a9c31f37a3183c008b7190b3976
tynew93 <tynew93@users.noreply.github.com>
2018-04-09 01:58:59 -0500

    Basic main menu
    Got a basic menu working. No settings yet, and the level select hasn't been implemented either.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,16 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
+    [Header("Set in Inspector")]
+    public AudioSource MainMenuTheme;
+
 	// Use this for initialization
 	void Start () {
-		
+	    MainMenuTheme.Play();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
 }
