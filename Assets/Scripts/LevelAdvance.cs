9bfa9ae946070232f94a582c58f37e7418d868a1
Daniel Norwood <31140068+DLNorwood94@users.noreply.github.com>
2018-04-20 00:30:41 -0500

    Added (Partial) Level Transition

---
+++ Assets/Scripts/LevelAdvance.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelAdvance : MonoBehaviour {
+
+	public GameObject[] vcams;
+	private int currentLevel;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag ("Player") && vcams [currentLevel].activeSelf) {
+			vcams [currentLevel].SetActive (false);
+			++currentLevel;
+			vcams [currentLevel].SetActive (true);
+			gameObject.SetActive (false);
+		}
+	}
+}
