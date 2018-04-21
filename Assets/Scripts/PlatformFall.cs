706c1d694eac993960cfe0fef71e544fa338ba1c
Daniel Norwood <31140068+DLNorwood94@users.noreply.github.com>
2018-04-21 18:29:57 -0500

    Working on Third Level

---
+++ Assets/Scripts/PlatformFall.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFall : MonoBehaviour {
+
+	public float fallDelay = 1f;
+	public float respawnDelay = 2f;
+	public GameObject platformPrefab;
+
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Awake () {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void OnCollisionEnter2D (Collision2D other)
+	{
+		if (other.gameObject.CompareTag ("Player"))
+			Invoke ("Fall", fallDelay);
+	}
+
+	void Fall ()
+	{
+		rb2d.isKinematic = false;
+		Invoke ("Respawn", respawnDelay);
+	}
+
+	void Respawn()
+	{
+		Destroy (gameObject);
+		Instantiate (platformPrefab);
+	}
+}
